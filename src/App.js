003670cac9ce6435c2365dfacbaae0d3822c24d7
ssat <svstechie@gmail.com>
2019-11-15 08:56:28 -0500

    SSatish: Initial commit for project setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component} from "react";
 import {hot} from "react-hot-loader";
 
 class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, World! </h1>
-      </div>
-    );
-  }
+    componentDidMount(){
+
+    }
+    render(){
+        return(
+        <div className="App">
+            <h1> Hello, World! </h1>
+            
+        </div>
+        );
+    }
 }
 
 export default hot(module)(App);
