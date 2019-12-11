dfa91042cd2372a39207a555fa43dba9f6f29a8b
gajenthiran <gajenthiran.m@e2infosystems.com>
2019-12-11 19:36:52 +0530

    Updated test cases

--- test/board-spec.js
+++ test/board-spec.js
@@ -34,4 +34,33 @@ describe("Board", () => {
            expect(a).to.equal(9);  
     });
 
+    it('check first click should print the value X', () => {
+        const component = mount(<Board />);
+        component.find('.cell').at(0).simulate('click');
+        expect(component.find('.cell').at(0).text()).to.equal('X');  
+    });
+
+    it('check second click should print the value O', () => {
+        const component = mount(<Board />);
+        component.find('.cell').at(0).simulate('click');
+        component.find('.cell').at(1).simulate('click');
+        expect(component.find('.cell').at(1).text()).to.equal('O');  
+    });
+
+    it('check alternative values should be print on each click starting with X ', () => {
+        const component = mount(<Board />);
+        let a=0;
+         component.find('.cell').forEach((node) => {
+                if(a%2 == 0) {
+                    component.find('.cell').at(a).simulate('click');
+                    expect(component.find('.cell').at(a).text()).to.equal('X');  
+
+                } else {
+                    component.find('.cell').at(a).simulate('click');
+                    expect(component.find('.cell').at(a).text()).to.equal('O');  
+                }
+           })  
+    });
+
+
 })
